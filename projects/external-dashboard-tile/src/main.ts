d86f171e34a765c0058979f0ee5b95b774d9a1ad
ManfredSteyer <manfred.steyer@gmx.net>
2018-07-07 00:18:22 +0200

    added noop zone

--- projects/external-dashboard-tile/src/main.ts
+++ projects/external-dashboard-tile/src/main.ts
@@ -8,5 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { ngZone: 'noop' })
   .catch(err => console.log(err));
