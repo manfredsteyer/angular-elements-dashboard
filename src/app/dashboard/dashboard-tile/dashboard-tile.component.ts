7af1269248a4ec4a93356754dd00ebdbc55763a1
ManfredSteyer <manfred.steyer@gmx.net>
2018-07-07 14:52:30 +0200

    only use ngx-charts in main app

--- src/app/dashboard/dashboard-tile/dashboard-tile.component.ts
+++ src/app/dashboard/dashboard-tile/dashboard-tile.component.ts
@@ -5,8 +5,27 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './dashboard-tile.component.html',
   styleUrls: ['./dashboard-tile.component.css']
 })
-export class DashboardTileComponent  {
+export class DashboardTileComponent implements OnInit {
   @Input() a: number;
   @Input() b: number;
   @Input() c: number;
+
+  data: object = {};
+
+  ngOnInit() {
+    this.data = [
+      {
+        name: 'a',
+        value: this.a
+      },
+      {
+        name: 'b',
+        value: this.b
+      },     
+      {
+        name: 'c',
+        value: this.c
+      }
+    ];
+  }
 }
