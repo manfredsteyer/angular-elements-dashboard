0c3ba621a5a38bfa5020c9da376859135b98ad4f
ManfredSteyer <manfred.steyer@gmx.net>
2018-07-07 15:38:32 +0200

    added more button

--- projects/external-dashboard-tile/src/app/app.module.ts
+++ projects/external-dashboard-tile/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PushPipe } from './push.pipe';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { ExternalDashboardTileComponent } from './external-dashboard-tile/external-dashboard-tile.component';
@@ -8,7 +9,8 @@ import { createCustomElement } from '@angular/elements';
       BrowserModule
    ],
    declarations: [
-    ExternalDashboardTileComponent
+    ExternalDashboardTileComponent,
+    PushPipe
    ],
    bootstrap: [],
    entryComponents: [
--- projects/external-dashboard-tile/src/app/external-dashboard-tile/external-dashboard-tile.component.ts
+++ projects/external-dashboard-tile/src/app/external-dashboard-tile/external-dashboard-tile.component.ts
@@ -1,15 +1,43 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectorRef, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Subject, BehaviorSubject } from 'rxjs';
+
+class Stats {
+  constructor(
+    readonly a: number,
+    readonly b: number,
+    readonly c: number
+  ) { }
+}
 
 @Component({
-  // selector: 'app-external-dashboard-tile',
   templateUrl: './external-dashboard-tile.component.html',
-  styleUrls: ['./external-dashboard-tile.component.css']
+  styleUrls: ['./external-dashboard-tile.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ExternalDashboardTileComponent {
+export class ExternalDashboardTileComponent implements OnInit {
 
   @Input() a: number;
   @Input() b: number;
   @Input() c: number;
+
+  private statsSubject = new BehaviorSubject<Stats>(null);
+  public stats$ = this.statsSubject.asObservable();
+
+  // constructor(private cd: ChangeDetectorRef) {
+  // }
+
+  ngOnInit(): void {
+    this.statsSubject.next(new Stats(this.a, this.b, this.c));
+  }
+
+  more(): void {
+    this.a = Math.round(Math.random() * 100);
+    this.b = Math.round(Math.random() * 100);
+    this.c = Math.round(Math.random() * 100);
+
+    // this.cd.markForCheck();
+    this.statsSubject.next(new Stats(this.a, this.b, this.c));
+  }
 
 }
 
